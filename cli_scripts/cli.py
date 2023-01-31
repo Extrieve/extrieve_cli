146aa40ba57b060b6babfe295d1b3994fa8b5c44
Nick <nicolas.cinera@gmail.com>
2023-01-31 16:43:30 -0500

    Better removal implementation

--- cli_scripts/cli.py
+++ cli_scripts/cli.py
@@ -5,6 +5,18 @@ from cli_scripts import __appname__, __version__
 
 app = typer.Typer()
 
+# let's define a function that access every file within a directory
+# recursively and if it matchnes the pattern remove it.
+def _access_and_delete_files(directory: str, pattern: str) -> None:
+    for file in os.listdir(directory):
+        if os.path.isdir(file):
+            _access_and_delete_files(file)
+        else:
+            if pattern in file:
+                os.remove(file)
+                typer.echo(f"File {file} removed")
+
+
 @app.command(help = "Clean a directory", name = "clean", no_args_is_help = True)
 def clean_directory(
         directory: str = typer.Argument(
@@ -33,26 +45,26 @@ def clean_directory(
     os.chdir(directory)
 
     # if not folder remove all folders in directory
-    if not folder_to_remove:
-        for folder in os.listdir():
-            if os.path.isdir(folder) and not file_to_remove:
-                shutil.rmtree(folder)
-                # log to user
-                typer.echo(f"Removed {folder}")
-            elif os.path.isdir(folder) and file_to_remove:
-                for file in os.listdir(folder):
-                    if file == file_to_remove:
-                        os.remove(os.path.join(folder, file))
-            else:
-                continue
-
-    if folder_to_remove:
-        for folder in os.listdir():
-            if folder_to_remove in folder:
-                shutil.rmtree(folder)
-            else:
-                continue
+    if not folder_to_remove and file_to_remove:
+        # remove all files within each folder in directory
+        _access_and_delete_files(directory)
     
+    elif folder_to_remove and not file_to_remove:
+        # remove all files within each folder in directory that matches folder_to_remove
+        for folder in os.listdir(directory):
+            if os.path.isdir(folder) and folder_to_remove in folder:
+                shutil.rmtree(folder)
+
+    elif folder_to_remove and file_to_remove:
+        # remove all files within each folder in directory that matches folder_to_remove
+        for folder in os.listdir(directory):
+            if os.path.isdir(folder) and folder_to_remove in folder:
+                _access_and_delete_files(folder, file_to_remove)
+
+    else:
+        typer.echo("You need to specify at least one option")
+        raise typer.Exit(1)
+
 def _version_callback(value: bool):
     if value:
         typer.echo(f"{__appname__} version {__version__}")
